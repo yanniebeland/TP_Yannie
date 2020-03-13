756a3214bfca7891cd376975172bb077c5ebc848
yanniebeland <62118951+yanniebeland@users.noreply.github.com>
2020-03-13 13:07:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import argparse
+
+
+def analyser_commande():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+
+def main():
+    args = analyser_commande()
+
+
+if __name__ == '__main__':
+    main()
 
