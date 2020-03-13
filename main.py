bf9c91f1f2a2eeddf23243afacb2fbc910e7abd0
yanniebeland <62118951+yanniebeland@users.noreply.github.com>
2020-03-13 13:38:27 -0400

    nouveau argparse

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import argparse
 
 
 def analyser_commande():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Jeu Quoridor - phase 1')
+    parser.add_argument('IDUL', metavar='IDUL',
+                        default='yabel34', help="IDUL du joueur")
+    parser.add_argument('-l', '--lister', dest='liste',
+                        help='Lister les identifiants de vos 20 dernières parties.')
     return parser.parse_args()
 
 
 def main():
-    args = analyser_commande()
+    arguments = analyser_commande()
 
 
 if __name__ == '__main__':
