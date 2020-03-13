27e81f46e3afe13de212e766f82b86f8e68dd384
yanniebeland <62118951+yanniebeland@users.noreply.github.com>
2020-03-13 14:37:11 -0400

    fonction lister_parties

--- api.py
+++ api.py
@@ -1,14 +1,22 @@
 import requests
+import json
 
 
-url_base = 'https://python.gel.ulaval.ca/quoridor/api'
-
-rep = requests.get('{}/lister/'.format(url_base), params={'idul': 'yabel34'})
-if rep.status_code == 200:
-    rep = rep.json()
-    print(rep)  
-else:
-    print("Le GET sur '{}lister' a produit le code d'erreur {}".format(
-        url_base, erp.status_code)
-    )
+def lister_parties(idul):
+    """
+    (Qu'est-ce que fait ma fonction?)
+    (Quels sont les paramètres en entrée de ma fonction) idul
+    (Quelle est la sortie et/ou le retour de ma fonction)
+    """
+    url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
+    donnees = requests.get(url_lister, params={'idul': idul})
+    if donnees.status_code == 200:
+        donnees = donnees.text
+        json_x = json.loads(donnees)
+        json_y = json.dumps(json_x, indent=2)
+        print(json_y)  
+    else:
+        print("Le GET sur '{}' a produit le code d'erreur {}".format(
+            url_lister, donnees.status_code)
+        )
 
